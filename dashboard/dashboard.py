2028af7ded61eb8dfaa01bf6204b69949e75e2cb
astriwidyastiti <112534966+astriwidyastiti@users.noreply.github.com>
2023-11-22 21:29:17 +0700

    dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -40,7 +40,7 @@ def create_rfm_df(df):
     rfm_df.drop("max_order_timestamp", axis=1, inplace=True)
     return rfm_df
 # Prepare dataframe
-day_df = pd.read_csv("dashboard\day_df.csv")
+day_df = pd.read_csv("dashboard/day_df.csv")
 
 datetime_columns = ["date"]
 day_df.sort_values(by="date", inplace=True)
